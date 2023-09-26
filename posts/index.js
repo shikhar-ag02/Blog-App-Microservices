7bca144e9f044f945b7377247c4a3f7a1749fd6a
Shikhar <shikhar.ag02@gmail.com>
2023-09-27 01:01:28 +0530

    Added a event- bus pod and deployement. Created cluster services for posts and event-bus to allow communication between pods. Node port service is created to allow communication from outside the cluster

--- posts/index.js
+++ posts/index.js
@@ -21,7 +21,7 @@ app.post("/posts", async (req, res) => {
     title,
   };
   //Event schema: Type(of the event) & Data
-  await axios.post("http://localhost:4005/events", {
+  await axios.post("http://event-bus-srv:4005/events", {
     type: "PostCreated",
     data: {
       id,
